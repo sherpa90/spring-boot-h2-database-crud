e16dc9436af1d2574361f5b2fcc8f48233c67af9
mrosas <marco.rosasvillarroel@gmail.com>
2024-08-27 20:32:30 -0400

    commit de reparacion

---
+++ src/main/java/com/bezkoder/spring/jpa/h2/Calculadora.java
@@ -0,0 +1,12 @@
+package com.bezkoder.spring.jpa.h2;
+
+public class Calculadora {
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+}
+
